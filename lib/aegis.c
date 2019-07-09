9fa54ba9f14927eaa3613373c912df67ef0774d1
Brendan Christy <toxitygamer@gmail.com>
2019-07-09 12:32:21 +0200

    Adding the library header and function stubs for the API

---
+++ lib/aegis.c
@@ -0,0 +1,29 @@
+#include "aegis.h"
+
+uint32_t draw_line(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, Color col) {
+    return 0;
+}
+
+uint32_t draw_circle(uint32_t x, uint32_t y, uint32_t r, Color col) {
+    return 0;
+}
+
+uint32_t draw_ellipse(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, Color col) {
+    return 0;
+}
+
+uint32_t fill_rect(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, Color col) {
+    return 0;
+}
+
+uint32_t copy_font(uint32_t* font) {
+    return 0;
+}
+
+uint32_t draw_font(uint32_t x, uint32_t y, char letter, Color col) {
+    return 0;
+}
+
+uint32_t draw_sprite(uint32_t x, uint32_t y, uint32_t* sprite, uint32_t* mask) {
+    return 0;
+}
---
+++ lib/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "aegis.h"
+
+int main(int argc, char** argv) {
+
+    unsigned int col = COLOR(2047,2047,1023);
+    printf("%x\n", col);
+    
+
+
+    return 0;
+}
